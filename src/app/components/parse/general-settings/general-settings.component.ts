9bab97dba3d043845e2163128fa314c87f93e9ad
Silas Meier <silas.meier@bluewin.ch>
2022-05-20 15:07:04 +0200

    General settings component added

---
+++ src/app/components/parse/general-settings/general-settings.component.ts
@@ -0,0 +1,33 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-general-settings',
+  templateUrl: './general-settings.component.html',
+  styleUrls: ['./general-settings.component.css']
+})
+export class GeneralSettingsComponent implements OnInit {
+
+  showBoundingBox: boolean = false;
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+  unload(): void {
+
+  }
+
+  changeBackground(): void {
+
+  }
+
+  setBoundingBox(): void{
+
+  }
+
+  start() {
+
+  }
+
+}
